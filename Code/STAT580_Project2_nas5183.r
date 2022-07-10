e7a5de24bf525b484ef71ea5c11f32ce5174c83e
Sweigalytics <neilsweigard@gmail.com>
2022-07-10 12:00:43 -0400

    Submission for training data

--- Code/STAT580_Project2_nas5183.r
+++ Code/STAT580_Project2_nas5183.r
@@ -1,3 +1,4 @@
+library(caret) # For dummyVars()
 library(ggpubr)
 library(tidyverse)
 
@@ -30,14 +31,14 @@ nrow(OldTown) / (nrow(OldTown) + nrow(OldTown_test)) # 0.8018018
 
 
 # Separate the columns with multiple delimited values (`Exterior` and `LotInfo`) into separate columns.
-exterior_cols = c("ExteriorMetalSd","ExteriorOtherSd","ExteriorVinylSd")
-lot_cols = c("LotType","LotShape","LotFR2","LotFR3")
+exterior_cols = c("Exterior1st","ExteriorQual","ExteriorCond")
+lot_cols = c("LotConfig","LotShape","LotArea","LotFrontage")
 
 df_neighborhoods_separate <- df_neighborhoods %>% separate(Exterior, exterior_cols, sep=";") %>% separate(LotInfo, lot_cols, sep=";")
 df_neighborhoods_separate_test <- df_neighborhoods_test %>% separate(Exterior, exterior_cols, sep=";") %>% separate(LotInfo, lot_cols, sep=";")
 
-df_neighborhoods_separate[,c("LotFR2","LotFR3")] <- sapply(df_neighborhoods_separate[,c("LotFR2","LotFR3")], as.integer)
-df_neighborhoods_separate_test[,c("LotFR2","LotFR3")] <- sapply(df_neighborhoods_separate_test[,c("LotFR2","LotFR3")], as.integer)
+df_neighborhoods_separate[,c("LotArea","LotFrontage")] <- sapply(df_neighborhoods_separate[,c("LotArea","LotFrontage")], as.integer)
+df_neighborhoods_separate_test[,c("LotArea","LotFrontage")] <- sapply(df_neighborhoods_separate_test[,c("LotArea","LotFrontage")], as.integer)
 
 
 # Fill in "NA" for empty strings in `BsmtQual`, `BsmtFinType1`, and `GarageType`.
@@ -45,11 +46,14 @@ df_neighborhoods_separate_test[,c("LotFR2","LotFR3")] <- sapply(df_neighborhoods
 empty_string_cols <- c("BsmtQual","BsmtFinType1","GarageType")
 df_neighborhoods_impute <- df_neighborhoods_separate %>% 
                             mutate_at(empty_string_cols, ~replace(., . == "", "NA")) %>%
-                            mutate_at("LotFR3", ~replace(., is.na(.), 0))
+                            mutate_at("BsmtCond", ~replace(., is.na(.), "NA")) %>%
+                            mutate_at("LotFrontage", ~replace(., is.na(.), 0)
+                            )
 df_neighborhoods_impute_test <- df_neighborhoods_separate_test %>% 
                                   mutate_at(empty_string_cols, ~replace(., . == "", "NA")) %>%
-                                  mutate_at("LotFR3", ~replace(., is.na(.), 0))
-
+                                  mutate_at("BsmtCond", ~replace(., is.na(.), "NA")) %>%
+                                  mutate_at("LotFrontage", ~replace(., is.na(.), 0)
+                                  )
 
 
 # Plot histograms for numeric columns
@@ -69,8 +73,8 @@ ggarrange(
   plot_FullBath,
   plot_GrLivArea,
   plot_HalfBath,
-  plot_LotFR2,
-  plot_LotFR3,
+  plot_LotArea,
+  plot_LotFrontage,
   plot_OpenPorchSF,
   plot_OverallCond,
   plot_OverallQual,
@@ -80,3 +84,57 @@ ggarrange(
   plot_YearBuilt,
   plot_YrSold
 )
+
+# Removing the record with `YrSold` of 2001 because it is an error.
+# Also removing the `Utilities` column because it only has one value.
+df_neighborhoods_cleaned <- df_neighborhoods_impute[-which(df_neighborhoods_impute$YrSold == 2001) , !names(df_neighborhoods_impute) == "Utilities"]
+df_neighborhoods_cleaned_test <- df_neighborhoods_impute_test[ , !names(df_neighborhoods_impute) == "Utilities"] # There is no `YrSold` == 2001 record to remove from the test data.
+
+
+df_neighborhoods_scaled <- df_neighborhoods_cleaned %>% mutate_if(is.numeric, scale)
+df_neighborhoods_scaled_test <- df_neighborhoods_cleaned_test %>% mutate_if(is.numeric, scale)
+
+num_cols <- colnames(select_if(df_neighborhoods_scaled, is.numeric))
+
+for(i in num_cols){
+  
+  assign(paste('plot_scaled_',i,sep=""), 
+         ggplot(data=df_neighborhoods_scaled, aes_string(x=i)) + geom_histogram() + theme(text = element_text(size = 8))
+  )
+}
+
+ggarrange(
+  plot_scaled_BedroomAbvGr,
+  plot_scaled_BsmtFinSF1,
+  plot_scaled_Fireplaces,
+  plot_scaled_FullBath,
+  plot_scaled_GrLivArea,
+  plot_scaled_HalfBath,
+  plot_scaled_LotArea,
+  plot_scaled_LotFrontage,
+  plot_scaled_OpenPorchSF,
+  plot_scaled_OverallCond,
+  plot_scaled_OverallQual,
+  plot_scaled_SalePrice,
+  plot_scaled_TotRmsAbvGrd,
+  plot_scaled_WoodDeckSF,
+  plot_scaled_YearBuilt,
+  plot_scaled_YrSold
+)
+
+# Not needed for now, but will find the names of non-numeric columns.
+# non_num_cols <- colnames(select_if(df_neighborhoods_scaled, negate(is.numeric)))
+
+# Encodes the categorical 
+dmy <- dummyVars(" ~ .", data = df_neighborhoods_scaled[, !names(df_neighborhoods_scaled) == "SalePrice"])
+df_neighborhoods_final <- cbind(data.frame(predict(dmy, newdata = df_neighborhoods_scaled)), df_neighborhoods_scaled$SalePrice)
+names(df_neighborhoods_final)[names(df_neighborhoods_final) == "df_neighborhoods_scaled$SalePrice"] <- "SalePrice"
+write.csv(df_neighborhoods_final, '../df_neighborhoods_final.csv')
+
+df_neighborhoods_final_test <- data.frame(predict(dmy, newdata = df_neighborhoods_scaled_test))
+
+# Saving this code in case we need to just change variables to factors instead of encoding.
+# str(df_neighborhoods_scaled %>% mutate_if(negate(is.numeric), as.factor))
+
+
+str(df_neighborhoods_scaled_test %>% mutate_if(negate(is.numeric), as.factor))
