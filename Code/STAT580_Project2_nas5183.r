ca582057beb32078dff58bf1a560a8200b5760cd
Sweigalytics <neilsweigard@gmail.com>
2022-07-24 15:36:38 -0400

    Added predictions and combined MSE
    We now have predictions for 10 different models to compare.

--- Code/STAT580_Project2_nas5183.r
+++ Code/STAT580_Project2_nas5183.r
@@ -1,10 +1,15 @@
 library(caret) # For dummyVars()
+library(e1071) # For SVR
 library(glmnet) # For Ridge Regression and Lasso
 library(ggpubr)
 library(leaps) # For regsubsets()
 library(tidyverse)
 
-# Read in the source data files.
+###############################################
+# 1. Data Ingestion, Cleaning, and Feature Engineering
+###############################################
+
+## Read in the source data files.
 CollegeCr <- read.csv('../Data/CollegeCr.csv')
 CollegeCr_test <- read.csv('../Data/CollegeCr.test.csv')
 Edwards <- read.csv('../Data/Edwards.csv')
@@ -12,12 +17,12 @@ Edwards_test <- read.csv('../Data/Edwards.test.csv')
 OldTown <- read.csv('../Data/OldTown.csv')
 OldTown_test <- read.csv('../Data/OldTown.test.csv')
 
-# Find the common column names between the files to combine.
+## Find the common column names between the files to combine. We will only include columns that are present in all three datasets.
 intersect_colnames <- sort(Reduce(intersect,list(colnames(CollegeCr),colnames(Edwards),colnames(OldTown))))
 intersect_colnames_test <- sort(Reduce(intersect,list(colnames(CollegeCr_test),colnames(Edwards_test),colnames(OldTown_test))))
 
-# Combine the neighborhoods into a common dataframe.
-# Retain their neighborhood name into a variable named `Neighborhood`
+## Combine the neighborhoods into a common dataframe.
+## Retain their neighborhood name into a variable named `Neighborhood`
 df_neighborhoods <- rbind(CollegeCr[,intersect_colnames] %>% mutate(Neighborhood = "CollegeCr"),
       Edwards[,intersect_colnames] %>% mutate(Neighborhood = "Edwards")
       ,OldTown[,intersect_colnames] %>% mutate(Neighborhood = "OldTown"))
@@ -26,13 +31,13 @@ df_neighborhoods_test <- rbind(CollegeCr_test[,intersect_colnames_test] %>% muta
                                Edwards_test[,intersect_colnames_test] %>% mutate(Neighborhood = "Edwards")
                                ,OldTown_test[,intersect_colnames_test] %>% mutate(Neighborhood = "OldTown"))
 
-# The training/test split between neighborhoods is slightly imbalanced.
+## The training/test split between neighborhoods is slightly imbalanced.
 nrow(CollegeCr) / (nrow(CollegeCr) + nrow(CollegeCr_test)) # 0.7945205
 nrow(Edwards) / (nrow(Edwards) + nrow(Edwards_test)) # 0.8314607
 nrow(OldTown) / (nrow(OldTown) + nrow(OldTown_test)) # 0.8018018
 
 
-# Separate the columns with multiple delimited values (`Exterior` and `LotInfo`) into separate columns.
+## Separate the columns with multiple delimited values (`Exterior` and `LotInfo`) into separate columns.
 exterior_cols = c("Exterior1st","ExteriorQual","ExteriorCond")
 lot_cols = c("LotConfig","LotShape","LotArea","LotFrontage")
 
@@ -42,11 +47,11 @@ df_neighborhoods_separate_test <- df_neighborhoods_test %>% separate(Exterior, e
 df_neighborhoods_separate[,c("LotArea","LotFrontage")] <- sapply(df_neighborhoods_separate[,c("LotArea","LotFrontage")], as.integer)
 df_neighborhoods_separate_test[,c("LotArea","LotFrontage")] <- sapply(df_neighborhoods_separate_test[,c("LotArea","LotFrontage")], as.integer)
 
-# De-duplicating records (rows 115, 116, and 279 are duplicates). We only perform this for training data because we do not want to remove records from test data.
+## De-duplicating records (rows 115, 116, and 279 are duplicates). We only perform this for training data because we do not want to remove records from test data.
 df_neighborhoods_dedupe <- unique(df_neighborhoods_separate)
 
-# Fill in "NA" for empty strings in `BsmtQual`, `BsmtFinType1`, and `GarageType`.
-# Also replace the empty `LotFR3` columns with 0. We will assume they do not have frontage on 3 sides.
+## Fill in "NA" for empty strings in `BsmtQual`, `BsmtFinType1`, and `GarageType`.
+## Also replace the empty `LotFR3` columns with 0. We will assume they do not have frontage on 3 sides.
 empty_string_cols <- c("BsmtQual","BsmtFinType1","GarageType")
 df_neighborhoods_impute <- df_neighborhoods_dedupe %>% 
                             mutate_at(empty_string_cols, ~replace(., . == "", "NA")) %>%
@@ -60,7 +65,7 @@ df_neighborhoods_impute_test <- df_neighborhoods_separate_test %>%
                                   )
 
 
-# Plot histograms for numeric columns
+## Plot histograms for numeric columns
 num_cols <- colnames(select_if(df_neighborhoods_impute, is.numeric))
 
 for(i in num_cols){
@@ -89,15 +94,31 @@ ggarrange(
   plot_YrSold
 )
 
-# Removing the record with `YrSold` of 2001 because it is an error.
-# Also removing the `Utilities` column because it only has one value.
+## Removing the record with `YrSold` of 2001 because it is an error. Its `YrSold` is too many years prior to `YearBuilt`.
+## Also removing the `Utilities` column because it only has one value and provides no predictive value.
 df_neighborhoods_cleaned <- df_neighborhoods_impute[-which(df_neighborhoods_impute$YrSold == 2001) , !names(df_neighborhoods_impute) == "Utilities"]
 df_neighborhoods_cleaned_test <- df_neighborhoods_impute_test[ , !names(df_neighborhoods_impute) == "Utilities"] # There is no `YrSold` == 2001 record to remove from the test data.
 
-# Encodes the categorical variables
+# Encodes the categorical variables for Linear Regression methods.
 dmy <- dummyVars(" ~ .", data = df_neighborhoods_cleaned[, !names(df_neighborhoods_cleaned) == "SalePrice"])
 df_neighborhoods_final <- cbind(data.frame(predict(dmy, newdata = df_neighborhoods_cleaned)), df_neighborhoods_cleaned$SalePrice)
 names(df_neighborhoods_final)[names(df_neighborhoods_final) == "df_neighborhoods_cleaned$SalePrice"] <- "SalePrice"
+
+set.seed(580)
+spec = c(train = .75, validate = .25)
+
+g = sample(cut(
+  seq(nrow(df_neighborhoods_final)), 
+  nrow(df_neighborhoods_final)*cumsum(c(0,spec)),
+  labels = names(spec)
+))
+
+res = split(df_neighborhoods_final, g)
+# Setting the validation dataframe
+df_neighborhoods_final_validation <- res$validate
+# Setting the training dataframe
+df_neighborhoods_final_train <- res$train
+
 
 df_neighborhoods_final_test <- data.frame(predict(dmy, newdata = df_neighborhoods_cleaned_test))
 
@@ -108,6 +129,20 @@ dummyVarNames <- names(df_neighborhoods_final)[!(names(df_neighborhoods_final) %
 # Creating scaled versions for Ridge Regression and Lasso, excluding the dummy variable names
 df_neighborhoods_scaled <- cbind(df_neighborhoods_cleaned[, -which(names(df_neighborhoods_cleaned) %in% c('SalePrice'))] %>% mutate_if(is.numeric, scale) %>% mutate_if(is.character, as.factor), df_neighborhoods_cleaned$SalePrice)
 names(df_neighborhoods_scaled)[names(df_neighborhoods_scaled) == "df_neighborhoods_cleaned$SalePrice"] <- "SalePrice"
+
+set.seed(580)
+
+g = sample(cut(
+  seq(nrow(df_neighborhoods_scaled)), 
+  nrow(df_neighborhoods_scaled)*cumsum(c(0,spec)),
+  labels = names(spec)
+))
+
+res = split(df_neighborhoods_scaled, g)
+# Setting the validation dataframe
+df_neighborhoods_scaled_validation <- res$validate
+# Setting the training dataframe
+df_neighborhoods_scaled_train <- res$train
 
 df_neighborhoods_scaled_test <- df_neighborhoods_cleaned_test[, -which(names(df_neighborhoods_cleaned_test) %in% c('SalePrice'))] %>% mutate_if(is.numeric, scale) %>% mutate_if(is.character, as.factor)
 
@@ -155,17 +190,17 @@ ggarrange(
 str(df_neighborhoods_scaled_test %>% mutate_if(negate(is.numeric), as.factor))
 
 
+###############################################
+# 2. Variable Selection and fitting
+###############################################
 
-## Variable Selection
-
-
-# Forward Stepwise Selection
-regit.fwd <- regsubsets(SalePrice~.,data=df_neighborhoods_final, nvmax=85, method="forward")
+## Forward Stepwise Selection
+regit.fwd <- regsubsets(SalePrice~.,data=df_neighborhoods_final_train, nvmax=86, method="forward")
 regit.fwd.summary <- summary(regit.fwd)
 
-min_cp.fwd <- which.min(regit.fwd.summary$cp)
-min_bic.fwd <- which.min(regit.fwd.summary$bic)
-max_adjr2.fwd <- which.max(regit.fwd.summary$adjr2)
+min_cp.fwd <- which.min(regit.fwd.summary$cp) # 22 Variables
+min_bic.fwd <- which.min(regit.fwd.summary$bic) # 18 Variables
+max_adjr2.fwd <- which.max(regit.fwd.summary$adjr2) # 34 Variables
 
 plot(regit.fwd.summary$rss, xlab="Number of Variables", ylab="RSS", type="l")
 
@@ -178,13 +213,34 @@ points(min_cp.fwd, regit.fwd.summary$cp[min_cp.fwd], col="red", cex=2, pch=20)
 plot(regit.fwd.summary$bic, xlab="Number of Variables", ylab="BIC", type="l")
 points(min_bic.fwd, regit.fwd.summary$bic[min_bic.fwd], col="red", cex=2, pch=20)
 
-# Backward stepwise selection.
-regit.bwd <- regsubsets(SalePrice~.,data=df_neighborhoods_final, nvmax=85, method="backward")
+### Parsing the selected variables for each Cp, BIC, and Adj R^2 methods.
+linear_forward_cp_vars <- paste(names(which(regit.fwd.summary$outmat[min_cp.fwd, ] == "*")), collapse="+")
+linear_forward_bic_vars <- paste(names(which(regit.fwd.summary$outmat[min_bic.fwd, ] == "*")), collapse="+")
+linear_forward_adjr2_vars <- paste(names(which(regit.fwd.summary$outmat[max_adjr2.fwd, ] == "*")), collapse="+")
+
+### Fitting the linear models for the three selection criteria.
+linear_forward_cp_model <- lm(paste("SalePrice ~ ",linear_forward_cp_vars,sep=""), data = df_neighborhoods_final_train)
+linear_forward_bic_model <- lm(paste("SalePrice ~ ",linear_forward_bic_vars,sep=""), data = df_neighborhoods_final_train)
+linear_forward_adjr2_model <- lm(paste("SalePrice ~ ",linear_forward_adjr2_vars,sep=""), data = df_neighborhoods_final_train)
+
+### Storing the predictions for each model against the validation data.
+linear_forward_cp_model.pred <- predict(linear_forward_cp_model, df_neighborhoods_final_validation)
+linear_forward_bic_model.pred <- predict(linear_forward_bic_model, df_neighborhoods_final_validation)
+linear_forward_adjr2_model.pred <- predict(linear_forward_adjr2_model, df_neighborhoods_final_validation)
+
+#### Calculating MSE for each model
+mse_linear_forward_cp_model <- c("Linear Forward Selected Cp",mean((linear_forward_cp_model.pred - df_neighborhoods_final_validation$SalePrice)^2)) # 1006790081
+mse_linear_forward_bic_model <- c("Linear Forward Selected BIC",mean((linear_forward_bic_model.pred - df_neighborhoods_final_validation$SalePrice)^2)) # 956447763
+mse_linear_forward_adjr2_model <- c("Linear Forward Selected Adj R^2",mean((linear_forward_adjr2_model.pred - df_neighborhoods_final_validation$SalePrice)^2)) # 1068484673
+
+
+## Backward stepwise selection.
+regit.bwd <- regsubsets(SalePrice~.,data=df_neighborhoods_final, nvmax=86, method="backward")
 regit.bwd.summary <- summary(regit.bwd)
 
-min_cp.bwd <- which.min(regit.bwd.summary$cp)
-min_bic.bwd <- which.min(regit.bwd.summary$bic)
-max_adjr2.bwd <- which.max(regit.bwd.summary$adjr2)
+min_cp.bwd <- which.min(regit.bwd.summary$cp) # 20 Variables
+min_bic.bwd <- which.min(regit.bwd.summary$bic) # 16 Variables
+max_adjr2.bwd <- which.max(regit.bwd.summary$adjr2) # 37 Variables
 
 plot(regit.bwd.summary$rss, xlab="Number of Variables", ylab="RSS", type="l")
 
@@ -197,10 +253,30 @@ points(min_cp.bwd, regit.bwd.summary$cp[min_cp.bwd], col="red", cex=2, pch=20)
 plot(regit.bwd.summary$bic, xlab="Number of Variables", ylab="BIC", type="l")
 points(min_bic.bwd, regit.bwd.summary$bic[min_bic.bwd], col="red", cex=2, pch=20)
 
+### Parsing the selected variables for each Cp, BIC, and Adj R^2 methods.
+linear_backward_cp_vars <- paste(names(which(regit.bwd.summary$outmat[min_cp.bwd, ] == "*")), collapse="+")
+linear_backward_bic_vars <- paste(names(which(regit.bwd.summary$outmat[min_bic.bwd, ] == "*")), collapse="+")
+linear_backward_adjr2_vars <- paste(names(which(regit.bwd.summary$outmat[max_adjr2.bwd, ] == "*")), collapse="+")
 
-# Ridge Regression
-x.train <- model.matrix(SalePrice~.,df_neighborhoods_scaled)[,-1]
-y.train <- df_neighborhoods_scaled$SalePrice
+### Fitting the linear models for the three selection criteria.
+linear_backward_cp_model <- lm(paste("SalePrice ~ ",linear_backward_cp_vars,sep=""), data = df_neighborhoods_final_train)
+linear_backward_bic_model <- lm(paste("SalePrice ~ ",linear_backward_bic_vars,sep=""), data = df_neighborhoods_final_train)
+linear_backward_adjr2_model <- lm(paste("SalePrice ~ ",linear_backward_adjr2_vars,sep=""), data = df_neighborhoods_final_train)
+
+### Storing the predictions for each model against the validation data.
+linear_backward_cp_model.pred <- predict(linear_backward_cp_model, df_neighborhoods_final_validation)
+linear_backward_bic_model.pred <- predict(linear_backward_bic_model, df_neighborhoods_final_validation)
+linear_backward_adjr2_model.pred <- predict(linear_backward_adjr2_model, df_neighborhoods_final_validation)
+
+### Calculating MSE for each model
+mse_linear_backward_cp_model <- c("Linear Backward Selected Cp",mean((linear_backward_cp_model.pred - df_neighborhoods_final_validation$SalePrice)^2)) # 902472200
+mse_linear_backward_bic_model <- c("Linear Backward Selected BIC",mean((linear_backward_bic_model.pred - df_neighborhoods_final_validation$SalePrice)^2)) # 867542533
+mse_linear_backward_adjr2_model <- c("Linear Backward Selected Adj R^2",mean((linear_backward_adjr2_model.pred - df_neighborhoods_final_validation$SalePrice)^2)) # 915621549
+
+
+## Ridge Regression
+x.train <- model.matrix(SalePrice~.,df_neighborhoods_scaled_train)[,-1]
+y.train <- df_neighborhoods_scaled_train$SalePrice
 
 grid=10^seq(10,-2,length=100)
 ridge.mod=glmnet(x.train,y.train,alpha=0,lambda=grid)
@@ -210,18 +286,21 @@ cv.out=cv.glmnet(x.train,y.train,alpha=0)
 plot(cv.out)
 
 bestlam=cv.out$lambda.min
-bestlam
+bestlam # 215004.4
 
 out=glmnet(x.train,y.train,alpha=0)
 predict(out,type="coefficients",s=bestlam)
 
-ridge.pred <- predict(ridge.mod, s = bestlam, newx = x.train)
-mean((ridge.pred - y.train)^2)
+x.validation <- model.matrix(SalePrice~.,df_neighborhoods_scaled_validation)[,-1]
+y.validation <- df_neighborhoods_scaled_validation$SalePrice
+
+ridge.pred <- predict(ridge.mod, s = bestlam, newx = x.validation)
+mse_ridge_regression <- c("Ridge Regression",mean((ridge.pred - y.validation)^2)) # 1,548,617,820
 
 
 # Lasso
-x.train <- model.matrix(SalePrice~.,df_neighborhoods_scaled)[,-1]
-y.train <- df_neighborhoods_scaled$SalePrice
+x.train <- model.matrix(SalePrice~.,df_neighborhoods_scaled_train)[,-1]
+y.train <- df_neighborhoods_scaled_train$SalePrice
 
 lasso.mod=glmnet(x.train,y.train,alpha=1,lambda=grid)
 plot(lasso.mod)
@@ -231,11 +310,71 @@ cv.out=cv.glmnet(x.train,y.train,alpha=1)
 plot(cv.out)
 
 bestlam=cv.out$lambda.min
-bestlam
+bestlam # 15525.06
 
 out=glmnet(x.train,y.train,alpha=1,lambda=grid)
 lasso.coef=predict(out,type="coefficients",s=bestlam)
 lasso.coef
 
-lasso.pred <- predict(lasso.mod, s = bestlam, newx = x.train)
-mean((lasso.pred - y.train)^2)
+x.validation <- model.matrix(SalePrice~.,df_neighborhoods_scaled_validation)[,-1]
+y.validation <- df_neighborhoods_scaled_validation$SalePrice
+
+lasso.pred <- predict(lasso.mod, s = bestlam, newx = x.validation)
+mse_lasso <- c("Lasso",mean((lasso.pred - y.validation)^2)) # 1,800,027,642
+
+
+# Elastic Net
+
+## Found a working example here: http://www.sthda.com/english/articles/37-model-selection-essentials-in-r/153-penalized-regression-essentials-ridge-lasso-elastic-net/#elastic-net
+
+## Set Train Control
+set.seed(580)
+train_control <- trainControl(method = "repeatedcv",
+                              number = 5,
+                              repeats = 5,
+                              search = "random",
+                              verboseIter = TRUE)
+
+elastic_train_data <- cbind(x.train, y.train)
+colnames(elastic_train_data)[colnames(elastic_train_data) == 'y.train'] <- 'SalePrice'
+
+elastic_net_model <- train(SalePrice ~ .,
+                           data = elastic_train_data,
+                           method = "glmnet",
+                           tuneLength = 25,
+                           trControl = train_control)
+
+x.validation <- model.matrix(SalePrice~.,df_neighborhoods_scaled_validation)[,-1]
+y.validation <- df_neighborhoods_scaled_validation$SalePrice
+
+elastic_net.pred <- elastic_net_model %>% predict(x.validation)
+mse_elastic_net <- c("Elastic Net",mean((elastic_net.pred - y.validation)^2)) #983,506,727.699282
+
+# Random Forest Regression
+library(randomForest)
+set.seed(580)
+
+rf.neighborhoods <- randomForest(SalePrice~., data = df_neighborhoods_final_train, mtry=85, importance=TRUE, ntree=100)
+rf.pred <- predict(rf.neighborhoods, newdata = df_neighborhoods_final_validation)
+mse_random_forest <- mean((rf.pred - y.validation)^2) #703846773
+
+importance(rf.neighborhoods)
+
+
+###############################################
+# Model Selection
+###############################################
+
+df_mse <- data.frame(rbind(mse_linear_forward_cp_model,
+                mse_linear_forward_bic_model,
+                mse_linear_forward_adjr2_model,
+                mse_linear_backward_cp_model,
+                mse_linear_backward_bic_model,
+                mse_linear_backward_adjr2_model,
+                mse_ridge_regression,
+                mse_lasso,
+                mse_elastic_net))
+
+colnames(df_mse) <- c("Model", "Mean Squared Error")
+df_mse$`Mean Squared Error` <- as.numeric(df_mse$`Mean Squared Error`)
+df_mse %>% arrange(`Mean Squared Error`)
